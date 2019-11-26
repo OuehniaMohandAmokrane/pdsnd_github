07c535d31eeca9e5b61a60b8456e3e7981c89e5c
Mohand Amokrane Ouehnia <ouehnia.mohand.amokrane@hotmail.com>
2019-11-26 21:08:20 +0100

    Adding project descreption into the bikeshare.py file.

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,30 @@
 import time
 import pandas as pd
 import numpy as np
+"""
+Our code is intended to provide the following information:
 
+		# 1 Popular times of travel (i.e., occurs most often in the start time)
+
+		most common month
+		most common day of week
+		most common hour of day
+		# 2 Popular stations and trip
+
+		most common start station
+		most common end station
+		most common trip from start to end (i.e., most frequent combination of start station and end station)
+		# 3 Trip duration
+
+		total travel time
+		average travel time
+		# 4 User info
+
+		counts of each user type
+		counts of each gender (only available for NYC and Chicago)
+		earliest, most recent, most common year of birth (only available for NYC and Chicago)
+
+"""
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
